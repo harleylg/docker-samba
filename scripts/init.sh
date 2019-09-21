57f7b3ea9ea0d04811802d223cb232a5f7b902d1
Harley Dragon <longzhf@visionfunds.com.cn>
2019-09-22 00:47:38 +0800

    Fixed bug for can't set user password

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,14 +5,14 @@ echo "$(env | grep -E '^USER_\d+=')" | while IFS= read -r I_USER ; do
     USER_NAME=$(echo "$I_USER" | awk -F= '{print $2}' | awk -F: '{print $1}')
     USER_PASSWORD=$(echo "$I_USER" | awk -F= '{print $2}' | awk -F: '{print $2}')
 
-    adduser -H -D -s /bin/false -u $USER_ID "$USER_NAME"
+    adduser -H -D -s /bin/false -u ${USER_ID} ${USER_NAME}
     chpasswd << EOF
-"${USER_NAME}":"${USER_PASSWORD}"
+${USER_NAME}:${USER_PASSWORD}
 EOF
 
-    smbpasswd -a -s "$USER_NAME" << EOF
-"${USER_PASSWORD}"
-"${USER_PASSWORD}"
+    smbpasswd -a -s ${USER_NAME} << EOF
+${USER_PASSWORD}
+${USER_PASSWORD}
 EOF
 
     unset $(echo "USER_"${USER_ID})
