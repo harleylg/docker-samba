1850e3273b9cc3ed5fa0b5671b90eebc500886e2
Harley Dragon <longzhf@visionfunds.com.cn>
2019-10-01 16:38:50 +0800

    Add user to samba group which gid=2000

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+SMB_GROUP=samba
+SMB_GID=2000
+
+addgroup -g ${SMB_GID} ${SMB_GROUP}
+
 echo "$(env | grep -E '^USER_\d+=')" | while IFS= read -r I_USER ; do
     USER_ID=$(echo "$I_USER" | awk -F= '{print $1}' | sed 's/USER_//g')
     USER_NAME=$(echo "$I_USER" | awk -F= '{print $2}' | awk -F: '{print $1}')
@@ -15,6 +20,7 @@ ${USER_PASSWORD}
 ${USER_PASSWORD}
 EOF
 
+    addgroup ${USER_NAME} ${SMB_GROUP}
     unset $(echo "USER_"${USER_ID})
 
 done
